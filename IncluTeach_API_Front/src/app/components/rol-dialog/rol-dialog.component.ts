7198642f84edfb63099d23516dcf41f7b6e220ab
anderson <jhon05jul@gmail.com>
2025-11-30 19:51:41 -0500

    realizacion de los roles

--- IncluTeach_API_Front/src/app/components/rol-dialog/rol-dialog.component.ts
+++ IncluTeach_API_Front/src/app/components/rol-dialog/rol-dialog.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-rol-dialog',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    FormsModule, 
+    MatDialogModule, 
+    MatButtonModule, 
+    MatFormFieldModule, 
+    MatInputModule
+  ],
   templateUrl: './rol-dialog.component.html',
-  styleUrl: './rol-dialog.component.css'
+  styleUrls: ['./rol-dialog.component.css']
 })
 export class RolDialogComponent {
+  private dialogRef = inject(MatDialogRef<RolDialogComponent>);
+  public data = inject(MAT_DIALOG_DATA);
 
+  rolData = this.data ? { ...this.data } : { nombre: '', descripcion: '' };
+  titulo = this.data ? 'Editar Rol' : 'Nuevo Rol';
+
+  guardar() {
+    if (this.rolData.nombre && this.rolData.descripcion) {
+      this.dialogRef.close(this.rolData);
+    }
+  }
+
+  cancelar() {
+    this.dialogRef.close(null);
+  }
 }
