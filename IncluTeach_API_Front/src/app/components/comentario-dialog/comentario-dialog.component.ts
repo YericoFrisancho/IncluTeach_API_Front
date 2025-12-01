d01698d6cf5882a1de1aa5e0e876bf406d718298
anderson <jhon05jul@gmail.com>
2025-11-30 19:40:58 -0500

    realizacion de los comentarios

--- IncluTeach_API_Front/src/app/components/comentario-dialog/comentario-dialog.component.ts
+++ IncluTeach_API_Front/src/app/components/comentario-dialog/comentario-dialog.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-comentario-dialog',
-  imports: [],
-  templateUrl: './comentario-dialog.component.html',
-  styleUrl: './comentario-dialog.component.css'
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatDialogModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  templateUrl: './comentario-dialog.component.html'
 })
 export class ComentarioDialogComponent {
+  private dialogRef = inject(MatDialogRef<ComentarioDialogComponent>);
+  public data = inject(MAT_DIALOG_DATA);
 
+  comentario = this.data ? { ...this.data } : { contenido: '' };
+  titulo = this.data ? 'Editar Comentario' : 'Nuevo Comentario';
+
+  guardar() {
+    if (this.comentario.contenido) {
+      this.dialogRef.close(this.comentario);
+    }
+  }
+  cancelar() { this.dialogRef.close(null); }
 }
