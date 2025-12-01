5894974b407e9c4b785bf1a75cf2f4ae6776f950
anderson <jhon05jul@gmail.com>
2025-11-30 19:49:38 -0500

    realizacion de los recursos

--- IncluTeach_API_Front/src/app/components/recurso-dialog/recurso-dialog.component.ts
+++ IncluTeach_API_Front/src/app/components/recurso-dialog/recurso-dialog.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { CategoriaService } from '../../services/categoria.service';
 
 @Component({
   selector: 'app-recurso-dialog',
-  imports: [],
-  templateUrl: './recurso-dialog.component.html',
-  styleUrl: './recurso-dialog.component.css'
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatDialogModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+  templateUrl: './recurso-dialog.component.html'
 })
-export class RecursoDialogComponent {
+export class RecursoDialogComponent implements OnInit {
+  private dialogRef = inject(MatDialogRef<RecursoDialogComponent>);
+  private catService = inject(CategoriaService);
+  public data = inject(MAT_DIALOG_DATA);
 
+  recurso = this.data ? { ...this.data } : { titulo: '', descripcion: '', categoriaId: null };
+  titulo = this.data ? 'Editar Recurso' : 'Nuevo Recurso';
+  categorias: any[] = [];
+
+  ngOnInit() {
+    this.catService.listar().subscribe(data => this.categorias = data);
+  }
+
+  guardar() {
+    if (this.recurso.titulo && this.recurso.descripcion && this.recurso.categoriaId) {
+      this.dialogRef.close(this.recurso);
+    }
+  }
+  cancelar() { this.dialogRef.close(null); }
 }
